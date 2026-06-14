0234ea5411660bd297664d50f064b8b0deb65db9
Christopher Bayliss <cbayliss512@gmail.com>
2026-06-14 13:59:06 -0500

    Initial Commit

---
+++ GroceryInventoryTracker/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GroceryInventoryTracker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
